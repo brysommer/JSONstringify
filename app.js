ccb9d602d58de47b1f0d6d3fc4aae9664e65be0b
brysommermac <brysommer1@gmail.com>
2022-10-13 19:57:11 +0300

    confused

--- app.js
+++ app.js
@@ -9,41 +9,51 @@ const port = 3000
 const MORALIS_API_KEY = "Litot1VeP1MVW8qGOZmPGMUcRQvCf4P0zg9JwhxXFSpAVzRMZzgZeQnW5JVP5zYX"
 const chain = EvmChain.ETHEREUM
 
-async function getDemoData() {
-  // Get native balance
-  const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
-    address,
-    chain,
-  })
+async function getDemoData () {
+        console.log('getdemo started');
+        // Get native balance
+        const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
+          address,
+          chain,
+        })
+      
+        // Format the native balance formatted in ether via the .ether getter
+        const native = nativeBalance.result.balance.ether
+      
+        // Get token balances
+        const tokenBalances = await Moralis.EvmApi.token.getWalletTokenBalances({
+          address,
+          chain,
+          limit: 10,
+        })
+      
+        // Format the balances to a readable output with the .display() method
+        const tokens = tokenBalances.result.map((token) => token.display())
+        console.log('getdemo finished');
+        return { native, tokens }
+        
+    }
 
-  // Format the native balance formatted in ether via the .ether getter
-  const native = nativeBalance.result.balance.ether
-
-  // Get token balances
-  const tokenBalances = await Moralis.EvmApi.token.getWalletTokenBalances({
-    address,
-    chain,
-    limit: 10,
-  })
-
-  // Format the balances to a readable output with the .display() method
-  const tokens = tokenBalances.result.map((token) => token.display())
-  return { native, tokens }
+const interval = ms => {
+    return new Promise(setInterval(getDemoData(), ms))
 }
 
-app.get("/demo", async (req, res) => {
-  try {
-
-    // Get and return the crypto data
-    const data = await getDemoData()
-    res.status(200)
-    res.json(data)
-  } catch (error) {
-    // Handle errors
-    console.error(error)
-    res.status(500)
-    res.json({ error: error.message })
-  }
+app.get("/demo",   (req, res) => {
+        async function printData () {
+            try {
+                // Get and return the crypto data
+               await interval();
+ //               const data =await getDemoData()
+                res.status(200)
+                res.json(data)
+              } catch (error) {
+                // Handle errors
+                console.error(error)
+                res.status(500)
+                res.json({ error: error.message })
+              }
+        } 
+       setInterval(printData, 3000);
 })
 
 const startServer = async () => {
